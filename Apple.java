5e726b33b1a33401ea2c2cd1ac8c801deece40de
sushmask <sushmask.xworkz@gmail.com>
2023-10-28 14:33:51 +0530

    apple

---
+++ Apple.java
@@ -0,0 +1,50 @@
+class Apple
+{
+
+public static void main(String[] values)
+
+{
+Apple apple;
+Orange orange;
+Glass glass;
+Chair chair;
+Table table;
+Bag bag;
+Lap lap;
+Door door;
+Tv tv;
+Pen pen;
+Pencil pencil;
+Thread thread;
+Clip clip;
+Watch watch;
+Band band;
+Jeans jeans;
+Top top;
+Keyboard keyboard;
+Mouse mouse;
+Bangles bangles;
+Purple purple;
+Grapes grapes;
+Paper paper;
+Book book;
+Shoes shoes;
+Car car;
+Bike bike;
+Cycle cycle;
+Plane  plane;
+Cover cover;
+Scotty scotty;
+Current current;
+Light light;
+Bulbe bulbe;
+People people;
+Dress dress;
+Washingmachine washingmachine;
+Mobile mobile;
+Black black;
+White white;
+Printer printer;
+
+}
+}
