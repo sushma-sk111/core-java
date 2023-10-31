019d7a0b4e260098873ba0799bedf8e231abe20d
sushmask <sushmask.xworkz@gmail.com>
2023-10-31 11:47:49 +0530

    showroom

---
+++ ShowroomStarter.java
@@ -0,0 +1,103 @@
+class ShowroomStarter
+{
+  public static void main(String args[])
+  {
+	  System.out.println("Starting showstarter");
+	  Showroom showroom=new Showroom();
+	  System.out.println("showroom:"+showroom.company);
+	System.out.println("showroom:"+showroom.noOfcars);
+	System.out.println("showroom:"+showroom.Managername);
+	System.out.println("showroom:"+showroom.cost);
+	System.out.println("showroom:"+showroom.name);
+	System.out.println("showroom:"+showroom.noOfemployes);
+	System.out.println("showroom:"+showroom.noOfworkers);
+	System.out.println("showroom:"+showroom.contactnumber);
+	
+	
+	 Showroom showroom1=new Showroom("Renult");
+	  System.out.println("showroom:"+showroom1.company);
+	System.out.println("showroom:"+showroom1.noOfcars);
+	System.out.println("showroom:"+showroom1.Managername);
+	System.out.println("showroom:"+showroom1.cost);
+	System.out.println("showroom:"+showroom1.name);
+	System.out.println("showroom:"+showroom1.noOfemployes);
+	System.out.println("showroom:"+showroom1.noOfworkers);
+	System.out.println("showroom:"+showroom1.contactnumber);
+	
+	 Showroom showroom2=new Showroom();
+	  System.out.println("showroom:"+showroom2.company);
+	System.out.println("showroom:"+showroom2.noOfcars);
+	System.out.println("showroom:"+showroom2.Managername);
+	System.out.println("showroom:"+showroom2.cost);
+	System.out.println("showroom:"+showroom2.name);
+	System.out.println("showroom:"+showroom2.noOfemployes);
+	System.out.println("showroom:"+showroom2.noOfworkers);
+	System.out.println("showroom:"+showroom2.contactnumber);
+	
+	 Showroom showroom3=new Showroom("Sush",20);
+	  System.out.println("showroom:"+showroom3.company);
+	System.out.println("showroom:"+showroom3.noOfcars);
+	System.out.println("showroom:"+showroom3.Managername);
+	System.out.println("showroom:"+showroom3.cost);
+	System.out.println("showroom:"+showroom3.name);
+	System.out.println("showroom:"+showroom3.noOfemployes);
+	System.out.println("showroom:"+showroom3.noOfworkers);
+	System.out.println("showroom:"+showroom3.contactnumber);
+	
+	 Showroom showroom4=new Showroom();
+	  System.out.println("showroom:"+showroom4.company);
+	System.out.println("showroom:"+showroom4.noOfcars);
+	System.out.println("showroom:"+showroom4.Managername);
+	System.out.println("showroom:"+showroom4.cost);
+	System.out.println("showroom:"+showroom4.name);
+	System.out.println("showroom:"+showroom4.noOfemployes);
+	System.out.println("showroom:"+showroom4.noOfworkers);
+	System.out.println("showroom:"+showroom4.contactnumber);
+	
+	
+	 Showroom showroom5=new Showroom("Shreya",9380543479L);
+	  System.out.println("showroom:"+showroom5.company);
+	System.out.println("showroom:"+showroom5.noOfcars);
+	System.out.println("showroom:"+showroom5.Managername);
+	System.out.println("showroom:"+showroom5.cost);
+	System.out.println("showroom:"+showroom5.name);
+	System.out.println("showroom:"+showroom5.noOfemployes);
+	System.out.println("showroom:"+showroom5.noOfworkers);
+	System.out.println("showroom:"+showroom5.contactnumber);
+	
+	 Showroom showroom6=new Showroom(6,"Sush");
+	  System.out.println("showroom:"+showroom6.company);
+	System.out.println("showroom:"+showroom6.noOfcars);
+	System.out.println("showroom:"+showroom6.Managername);
+	System.out.println("showroom:"+showroom6.cost);
+	System.out.println("showroom:"+showroom6.name);
+	System.out.println("showroom:"+showroom6.noOfemployes);
+	System.out.println("showroom:"+showroom6.noOfworkers);
+	System.out.println("showroom:"+showroom6.contactnumber);
+	
+	 Showroom showroom7=new Showroom();
+	  System.out.println("showroom:"+showroom7.company);
+	System.out.println("showroom:"+showroom7.noOfcars);
+	System.out.println("showroom:"+showroom7.Managername);
+	System.out.println("showroom:"+showroom7.cost);
+	System.out.println("showroom:"+showroom7.name);
+	System.out.println("showroom:"+showroom7.noOfemployes);
+	System.out.println("showroom:"+showroom7.noOfworkers);
+	System.out.println("showroom:"+showroom7.contactnumber);
+	
+	 Showroom showroom8=new Showroom();
+	  System.out.println("showroom:"+showroom8.company);
+	System.out.println("showroom:"+showroom8.noOfcars);
+	System.out.println("showroom:"+showroom8.Managername);
+	System.out.println("showroom:"+showroom8.cost);
+	System.out.println("showroom:"+showroom8.name);
+	System.out.println("showroom:"+showroom8.noOfemployes);
+	System.out.println("showroom:"+showroom8.noOfworkers);
+	System.out.println("showroom:"+showroom8.contactnumber);
+	
+	
+	
+	
+	
+  }
+}
