cc022d5937f2c58a3d77f64a3f006feff34dcada
sushmask <sushmask.xworkz@gmail.com>
2023-10-31 11:46:46 +0530

    matchbox

---
+++ MatchboxStarter.java
@@ -0,0 +1,65 @@
+class MatchboxStarter
+{
+  public static void main(String[] args)
+  {
+	  System.out.println("Starting matchboxstarter");
+  	Matchbox matchbox=new Matchbox();
+	System.out.println("matchbox:"+matchbox.company);
+	System.out.println("matchbox:"+matchbox.noOfsticks);
+	System.out.println("matchbox:"+matchbox.length);
+	System.out.println("matchbox:"+matchbox.price);
+	System.out.println("matchbox:"+matchbox.weight);
+	System.out.println("matchbox:"+matchbox.size);
+	
+	Matchbox matchbox1=new Matchbox("lotus");
+	System.out.println("matchbox:"+matchbox1.company);
+	System.out.println("matchbox:"+matchbox1.noOfsticks);
+	System.out.println("matchbox:"+matchbox1.length);
+	System.out.println("matchbox:"+matchbox1.price);
+	System.out.println("matchbox:"+matchbox1.weight);
+	System.out.println("matchbox:"+matchbox1.size);
+	
+	Matchbox matchbox2=new Matchbox(20);
+	System.out.println("matchbox:"+matchbox2.company);
+	System.out.println("matchbox:"+matchbox2.noOfsticks);
+	System.out.println("matchbox:"+matchbox2.length);
+	System.out.println("matchbox:"+matchbox2.price);
+	System.out.println("matchbox:"+matchbox2.weight);
+	System.out.println("matchbox:"+matchbox2.size);
+	
+	Matchbox matchbox3=new Matchbox();
+	System.out.println("matchbox:"+matchbox3.company);
+	System.out.println("matchbox:"+matchbox3.noOfsticks);
+	System.out.println("matchbox:"+matchbox3.length);
+	System.out.println("matchbox:"+matchbox3.price);
+	System.out.println("matchbox:"+matchbox3.weight);
+	System.out.println("matchbox:"+matchbox3.size);
+	
+	Matchbox matchbox4=new Matchbox(50,"lotus");
+	System.out.println("matchbox:"+matchbox4.company);
+	System.out.println("matchbox:"+matchbox4.noOfsticks);
+	System.out.println("matchbox:"+matchbox4.length);
+	System.out.println("matchbox:"+matchbox4.price);
+	System.out.println("matchbox:"+matchbox4.weight);
+	System.out.println("matchbox:"+matchbox4.size);
+	
+	Matchbox matchbox5=new Matchbox();
+	System.out.println("matchbox:"+matchbox5.company);
+	System.out.println("matchbox:"+matchbox5.noOfsticks);
+	System.out.println("matchbox:"+matchbox5.length);
+	System.out.println("matchbox:"+matchbox5.price);
+	System.out.println("matchbox:"+matchbox5.weight);
+	System.out.println("matchbox:"+matchbox5.size);
+	
+	Matchbox matchbox6=new Matchbox();
+	System.out.println("matchbox:"+matchbox6.company);
+	System.out.println("matchbox:"+matchbox6.noOfsticks);
+	System.out.println("matchbox:"+matchbox6.length);
+	System.out.println("matchbox:"+matchbox6.price);
+	System.out.println("matchbox:"+matchbox6.weight);
+	System.out.println("matchbox:"+matchbox6.size);
+	
+	 System.out.println("ending matchboxstarter");
+	
+  }
+}
