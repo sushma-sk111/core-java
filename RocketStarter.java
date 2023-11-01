091c5c1187a458a4b6251714a7faa3b6099ff6c4
sushmask <sushmask.xworkz@gmail.com>
2023-11-01 10:01:40 +0530

    Const

---
+++ RocketStarter.java
@@ -0,0 +1,39 @@
+class RocketStarter
+{
+	public static void main(String[] args)
+	{
+	    Rocket rocket=new Rocket();
+		System.out.println("weight:"+rocket.weight);
+		System.out.println("length:"+rocket.length);
+		System.out.println("noOffins:"+rocket.noOffins);
+		System.out.println("shapeOfnose:"+rocket.name);
+		
+		Rocket rocket1=new Rocket(13000);
+		System.out.println("weight:"+rocket1.weight);
+		System.out.println("length:"+rocket1.length);
+		System.out.println("noOffins:"+rocket1.noOffins);
+		System.out.println("shapeOfnose:"+rocket1.name);
+		
+		Rocket rocket2=new Rocket();
+		System.out.println("weight:"+rocket2.weight);
+		System.out.println("length:"+rocket2.length);
+		System.out.println("noOffins:"+rocket2.noOffins);
+		System.out.println("shapeOfnose:"+rocket2.name);
+		
+		Rocket rocket3=new Rocket();
+		System.out.println("weight:"+rocket3.weight);
+		System.out.println("length:"+rocket3.length);
+		System.out.println("noOffins:"+rocket3.noOffins);
+		System.out.println("shapeOfnose:"+rocket3.name);
+		
+		Rocket rocket4=new Rocket();
+		System.out.println("weight:"+rocket4.weight);
+		System.out.println("length:"+rocket4.length);
+		System.out.println("noOffins:"+rocket4.noOffins);
+		System.out.println("shapeOfnose:"+rocket4.name);
+		
+		
+		
+	}
+
+}
