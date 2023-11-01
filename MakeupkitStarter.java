64918db3ca5c17511521a6b19f8d6cc777ff5a64
sushmask <sushmask.xworkz@gmail.com>
2023-11-01 10:02:52 +0530

    Const

---
+++ MakeupkitStarter.java
@@ -0,0 +1,38 @@
+class MakeupkitStarter
+{
+	public static void main(String[] args)
+	{
+	    Makeupkit makeupkit=new Makeupkit();
+		System.out.println("company:"+makeupkit.company);
+		System.out.println("noOfitems:"+makeupkit.noOfitems);
+		System.out.println("cost:"+makeupkit.cost);
+		System.out.println(" noOfbrushes:"+makeupkit.noOfbrushes);
+		
+		Makeupkit makeupkit1=new Makeupkit();
+		System.out.println("company:"+makeupkit1.company);
+		System.out.println("noOfitems:"+makeupkit1.noOfitems);
+		System.out.println("cost:"+makeupkit1.cost);
+		System.out.println(" noOfbrushes:"+makeupkit1.noOfbrushes);
+		
+		Makeupkit makeupkit2=new Makeupkit();
+		System.out.println("company:"+makeupkit2.company);
+		System.out.println("noOfitems:"+makeupkit2.noOfitems);
+		System.out.println("cost:"+makeupkit2.cost);
+		System.out.println(" noOfbrushes:"+makeupkit2.noOfbrushes);
+		
+		Makeupkit makeupkit3=new Makeupkit();
+		System.out.println("company:"+makeupkit3.company);
+		System.out.println("noOfitems:"+makeupkit3.noOfitems);
+		System.out.println("cost:"+makeupkit3.cost);
+		System.out.println(" noOfbrushes:"+makeupkit3.noOfbrushes);
+		
+		Makeupkit makeupkit4=new Makeupkit();
+		System.out.println("company:"+makeupkit4.company);
+		System.out.println("noOfitems:"+makeupkit4.noOfitems);
+		System.out.println("cost:"+makeupkit4.cost);
+		System.out.println(" noOfbrushes:"+makeupkit4.noOfbrushes);
+		
+		
+	}
+
+}	
