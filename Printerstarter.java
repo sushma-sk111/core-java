31932873c6019339525df0566ff21695838f4a17
sushmask <sushmask.xworkz@gmail.com>
2023-11-02 10:06:35 +0530

    Const

---
+++ Printerstarter.java
@@ -0,0 +1,89 @@
+class Printerstarter
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Starting of main in Printerstarter");
+		Printer printer=new Printer();
+		System.out.println("cost:"+printer.cost);
+		System.out.println("company:"+printer.company);
+		System.out.println("noOfpages:"+printer.noOfpages);
+		System.out.println("color:"+printer.color);
+		System.out.println("productno:"+printer.productno);
+		
+		Printer printer1=new Printer();
+		System.out.println("cost:"+printer1.cost);
+		System.out.println("company:"+printer1.company);
+		System.out.println("noOfpages:"+printer1.noOfpages);
+		System.out.println("color:"+printer1.color);
+		System.out.println("productno:"+printer1.productno);
+		
+		Printer printer2=new Printer();
+		System.out.println("cost:"+printer2.cost);
+		System.out.println("company:"+printer2.company);
+		System.out.println("noOfpages:"+printer2.noOfpages);
+		System.out.println("color:"+printer2.color);
+		System.out.println("productno:"+printer2.productno);
+		
+		Printer printer3=new Printer();
+		System.out.println("cost:"+printer3.cost);
+		System.out.println("company:"+printer3.company);
+		System.out.println("noOfpages:"+printer3.noOfpages);
+		System.out.println("color:"+printer3.color);
+		System.out.println("productno:"+printer3.productno);
+		
+		Printer printer4=new Printer();
+		System.out.println("cost:"+printer4.cost);
+		System.out.println("company:"+printer4.company);
+		System.out.println("noOfpages:"+printer4.noOfpages);
+		System.out.println("color:"+printer4.color);
+		System.out.println("productno:"+printer4.productno);
+		
+		System.out.println("++++++++++++++++++");
+		
+		Helmet helmet=new Helmet();
+		System.out.println("company:"+helmet.company);
+		System.out.println("types:"+helmet.type);
+		System.out.println("cost:"+helmet.cost);
+		System.out.println("color:"+helmet.color);
+		System.out.println("features:"+helmet.features);
+		
+		Helmet helmet1=new Helmet();
+		System.out.println("company:"+helmet1.company);
+		System.out.println("types:"+helmet1.type);
+		System.out.println("cost:"+helmet1.cost);
+		System.out.println("color:"+helmet1.color);
+		System.out.println("features:"+helmet1.features);
+		
+		Helmet helmet2=new Helmet();
+		System.out.println("company:"+helmet2.company);
+		System.out.println("types:"+helmet2.type);
+		System.out.println("cost:"+helmet2.cost);
+		System.out.println("color:"+helmet2.color);
+		System.out.println("features:"+helmet2.features);
+		
+		Helmet helmet3=new Helmet();
+		System.out.println("company:"+helmet3.company);
+		System.out.println("types:"+helmet3.type);
+		System.out.println("cost:"+helmet3.cost);
+		System.out.println("color:"+helmet3.color);
+		System.out.println("features:"+helmet3.features);
+		
+		Helmet helmet4=new Helmet();
+		System.out.println("company:"+helmet4.company);
+		System.out.println("types:"+helmet4.type);
+		System.out.println("cost:"+helmet4.cost);
+		System.out.println("color:"+helmet4.color);
+		System.out.println("features:"+helmet4.features);
+		
+		Helmet helmet5=new Helmet();
+		System.out.println("company:"+helmet5.company);
+		System.out.println("types:"+helmet5.type);
+		System.out.println("cost:"+helmet5.cost);
+		System.out.println("color:"+helmet5.color);
+		System.out.println("features:"+helmet5.features);
+		
+		
+		
+		
+	}
+}
