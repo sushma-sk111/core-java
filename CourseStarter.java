3b1cc468f1941baeadf0915defae003693f6f4a5
sushmask <sushmask.xworkz@gmail.com>
2023-11-03 10:11:27 +0530

    COnstrctr

---
+++ CourseStarter.java
@@ -0,0 +1,30 @@
+class CourseStarter
+{
+	
+	public static void main(String args[])
+	{
+	Course course=new Course("Bca",3,"computer",4,8,"fulltime","BCA01");
+	course.displayinfo();
+	
+	Course course1=new Course("Mca",2,"computer",5,7,"fulltime","mCA01");
+	course1.displayinfo();
+	
+	Course course2=new Course("Bsc",4,"physics",6,8,"fulltime","Bsc01");
+	course2.displayinfo();
+	
+	Course course3=new Course("Msc",3,"chemistry",8,5,"fulltime","Msc01");
+	course3.displayinfo();
+	
+	Course course4=new Course("MA",3,"histry",4,8,"fulltime","MA01");
+	course4.displayinfo();
+	
+	Course course5=new Course("Bcom",3,"account",4,8,"fulltime","Bcom01");
+	course5.displayinfo();
+	
+	Course course6=new Course("Mcom",3,"Staticties",7,9,"fulltime","Mcom1");
+	course6.displayinfo();
+	
+	Course course7=new Course("pmcs",3,"computer",4,8,"fulltime","pmcs1");
+	course7.displayinfo();
+	}
+}
