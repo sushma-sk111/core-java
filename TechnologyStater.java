aaf5d7762f0bdaa701e90d4bbe190d7908a5872f
sushmask <sushmask.xworkz@gmail.com>
2023-11-03 10:10:35 +0530

    COnstrctr

---
+++ TechnologyStater.java
@@ -0,0 +1,46 @@
+class TechnologyStater
+{
+public static void main(String[] args)
+{
+Technology technology=new Technology("XWORKZ",5,"btm",24000,4.5,"java",3300009,"omkar",5,45,2,9.8);
+technology.infoShow();
+
+Technology technology1=new Technology("tap",7,"btm",30000,4.5,"java",3300009,"vinay",5,45,2,9.8);
+technology1.infoShow();
+
+
+Technology technology2=new Technology("kodnest",6,"rajajinagar",32000,4.7,"java,sql",3300009,"sanjay",5,45,2,11.8);
+technology2.infoShow();
+
+
+Technology technology3=new Technology("palle",5,"",28000,4.5,"python",4500098,"sunil",7,80,2,15.10);
+technology3.infoShow();
+
+
+Technology technology4=new Technology("jspider",7,"rajajinagar",40000,4.5,"java,sql,html",788067,"vinay",5,100,5,30);
+technology4.infoShow();
+
+
+Technology technology5=new Technology("qspider",6,"indira nagar",35000,4.0,"automation testing",3300009,"deepak",5,45,4,20);
+technology5.infoShow();
+
+Technology technology6=new Technology("besant",7,"btm",30000,4.5,"cloud computing",3300009,"mithun",5,120,2,16.5);
+technology6.infoShow();
+
+Technology technology7=new Technology("tap",7,"malleshwaram",30000,4.5,"vb.net",4500009,"vinay",5,45,3,12.4);
+technology7.infoShow();
+
+Technology technology8=new Technology("pentagon",8,"yashwanthpur",37000,4.2,"cyber security",110009,"manju",5,80,4,20);
+technology8.infoShow();
+
+Technology technology10=new Technology("XWORKZ",5,"rajajinagar",24000,4.5,"java",1100009,"omkar",5,45,2,9.8);
+technology10.infoShow();
+
+Technology technology11=new Technology("vx git",5,"jpnagar",50000,4.5,"ui/ux",550009,"kiran",5,60,2,30);
+technology11.infoShow();
+
+Technology technology12=new Technology("run tech",8,"jaynagar",60000,4.5,"softwaretesting",2500009,"sumith",5,50,2,17);
+technology12.infoShow();
+
+}
+}
