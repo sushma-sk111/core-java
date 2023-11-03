039d80162a0e319fb7d44c21446aa4af936f007c
sushmask <sushmask.xworkz@gmail.com>
2023-11-03 10:09:30 +0530

    COnstrctr

---
+++ PlacementStarter.java
@@ -0,0 +1,74 @@
+class PlacementStarter
+{
+	public static void main(String[] args)
+	{
+		Placement placement=new Placement();
+		placement.displayInfo();
+		
+		System.out.println("--------------------");
+		
+		Placement placement1=new Placement("X-workz","Banglore","java",4.5,2400,"amazon","placedrive",true,30,10,15,"Amulya");
+		placement1.displayInfo();
+		
+		System.out.println("--------------------");
+		
+		Placement placement2=new Placement("kodnest","davangere","html",4.5,2400,"flipcart","massinterview",true,30,10,15,"Akshara");
+        placement2.displayInfo();
+		
+		System.out.println("--------------------");
+		
+		Placement placement3=new Placement("basent","banglore","java-core",4.5,2400,"wipro","interview",true,35,25,20,"deepa");
+        placement3.displayInfo();
+		
+		System.out.println("--------------------");
+		
+		Placement placement4=new Placement("tap","btm","all",4.8,24000,"accenture","drive",false,36,250,200,"jaanu");
+        placement4.displayInfo();
+		
+		System.out.println("--------------------");
+		
+		Placement placement5=new Placement("basent","xceler","python",4.6,50000,"wipro","interview",true,50,27,28,"jaggu");
+        placement5.displayInfo();
+		
+		System.out.println("--------------------");
+		
+		Placement placement6=new Placement("Anglar","banglore","oop",4.9,2400,"viston","info",true,35,25,20,"vismaya");
+        placement6.displayInfo();
+		
+		System.out.println("--------------------");
+		
+		Placement placement7=new Placement("xceler","davangere","javaScript",4.5,2400,"infokra","view",false,35,25,20,"raksha");
+        placement7.displayInfo();
+		
+		System.out.println("--------------------");
+		
+		Placement placement8=new Placement("streek","banglore","java-core",5.5,3500,"wipro","interview",true,35,25,20,"Sindhu");
+        placement8.displayInfo();
+		
+		System.out.println("--------------------");
+		
+		Placement placement9=new Placement("abc","belagavi","sql",4.5,2400,"info","selecting",true,40,50,777,"Manu");
+        placement9.displayInfo();
+		
+		System.out.println("--------------------");
+		
+		Placement placement10=new Placement("basent","rajajinagar","html",4.5,2400,"tech","interview",true,44,55,88,"Sneha");
+        placement10.displayInfo();
+		
+		System.out.println("--------------------");
+		
+		Placement placement11=new Placement("basent","banglore","java-core",4.5,2400,"wipro","interview",true,35,25,20,"sushma");
+        placement11.displayInfo();
+		
+		System.out.println("--------------------");
+		
+		Placement placement12=new Placement("x-workz","rajajinagar","css",4.5,2400,"fokra","info",false,48,33,28,"teju");
+        placement12.displayInfo();
+		
+		
+		
+		
+		
+		
+	}
+}
