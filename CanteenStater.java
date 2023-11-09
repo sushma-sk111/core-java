35b9bba588ebab25a8945d3ca1bbf36893a77580
sushmask <sushmask.xworkz@gmail.com>
2023-11-09 10:28:34 +0530

    canten

---
+++ CanteenStater.java
@@ -0,0 +1,51 @@
+class CanteenStater
+{
+public static void main(String[] args)
+{
+Canteen canteen=new Canteen();
+String[] utensil=new String[40];
+utensil[1]="chefknife";
+utensil[2]="rod";
+utensil[3]="bowl";
+utensil[4]="spoon";
+utensil[5]="sieve";
+utensil[6]="grater";
+utensil[7]="roasting tin";
+utensil[8]="rolling pin";
+utensil[9]="Saucepans";
+utensil[10]="Masher";
+utensil[11]="bread bin";
+utensil[12]="pot holders";
+utensil[13]="KItchen foil";
+utensil[14]="pizza cutter";
+utensil[15]="ladle";
+utensil[16]="blender";
+utensil[17]="measuring cup";
+utensil[18]="measuring jug";
+utensil[19]="spatula";
+utensil[20]=" fork";
+utensil[21]="bread box";
+utensil[22]="toaster";
+utensil[23]="chefknife";
+utensil[24]="rod";
+utensil[25]="bowl";
+utensil[26]="spoon";
+utensil[27]="sieve";
+utensil[28]="grater";
+utensil[29]="roasting tin";
+utensil[30]="rolling pin";
+utensil[31]="Saucepans";
+utensil[32]="Masher";
+utensil[33]="grater";
+utensil[34]="roasting tin";
+utensil[35]="rolling pin";
+utensil[36]="Saucepans";
+utensil[37]="Masher";
+utensil[38]="bread bin";
+utensil[39]="pot holders";
+utensil[0]="KItchen foil";
+canteen.settype("veg");
+canteen.setutensils(utensil);
+canteen.showinfo();
+}
+}
